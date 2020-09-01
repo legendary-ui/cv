2350c27cc8725c9f470d9a8c6b530e192b4cbc2e
legendary-ui <68370863+legendary-ui@users.noreply.github.com>
2020-09-01 15:09:57 +0900

    Update index.js

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,14 +1,22 @@
 // interactive website
 
-for (var i = 0; i < 4; i++){
+for (var i = 0; i < 4; i++) {
   document.getElementsByClassName("download-button")[i].addEventListener("click", handleClick);
-  function handleClick(){
+
+  function handleClick() {
     document.getElementsByClassName("jsTextChange")[0].style.fontSize = "2.5rem"
     document.querySelector(".jsTextChange").innerHTML = "Thank you for using us! We would like your feed back on how our website was. Looking forward for more deals!";
   }
 }
 
-// review submit form
+document.getElementById("addContentButton").addEventListener("click", addContentFunc)
+
+function addContentFunc(){
+  document.getElementsByClassName("added-content")[0].classList.remove("added-content")
+}
+
+// add review
+
 var form = document.getElementById("jsReviewForm");
 
 form.onsubmit = function(e){
@@ -21,6 +29,14 @@ form.onsubmit = function(e){
 
   var carousel = document.getElementsByClassName("jsCarousel")[0];
   carousel.innerHTML = carousel.innerHTML + carouselFrame;
+
+  carouselItems = document.getElementsByClassName("Carousel-item");
+
+  for(var i=3; i=carouselItems.length; i++){
+    var indicators = document.getElementsByClassName("carousel-indicators")[0];
+    var addIndicator = "<li data-target='#carouselExampleControls' data-slide-to='"+ i +"'></li>";
+    indicators.innerHTML = indicators.innerHTML + addIndicator
+  }
 
   form.reset();
 }

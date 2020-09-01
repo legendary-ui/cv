1c9f0b265f332d7fedd27e4794b9f5b680c6e5f6
legendary-ui <68370863+legendary-ui@users.noreply.github.com>
2020-09-01 15:18:33 +0900

    Update index.js

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,13 +1,6 @@
 // interactive website
 
-for (var i = 0; i < 4; i++) {
-  document.getElementsByClassName("download-button")[i].addEventListener("click", handleClick);
 
-  function handleClick() {
-    document.getElementsByClassName("jsTextChange")[0].style.fontSize = "2.5rem"
-    document.querySelector(".jsTextChange").innerHTML = "Thank you for using us! We would like your feed back on how our website was. Looking forward for more deals!";
-  }
-}
 
 document.getElementById("addContentButton").addEventListener("click", addContentFunc)
 

b004f4110ee65de872884f5c6482b696c25d5cd1
legendary-ui <68370863+legendary-ui@users.noreply.github.com>
2020-10-20 15:43:48 +0900

    Update me.js

--- Javascript/me.js
+++ Javascript/me.js
@@ -1,22 +1,37 @@
-for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+
+
+
+$("#jsPasswordCheck").click(function(){
+  var givenPassword = prompt("Enter Password");
+  if (givenPassword == "123"){
+    window.location.href = "file:///C:/Users/moham/Desktop/FullStackWeb/discord.html"
+  } else{
+    alert("access denied")
+  }
+})
+
+for (var i = 0; i < 4; i++) {
   document.querySelectorAll(".jsAddContentAboutMe")[i].addEventListener("click", function(){
 
     var textInsideButton = this.innerHTML;
 
     switch (textInsideButton) {
       case "See My Hobbies!":
-        document.querySelectorAll(".addedContentHobbies")[0].classList.remove("added-content");
-        document.querySelectorAll(".jsAddedContentAboutMe")[0].classList.remove("jsAddedContentAboutMe");
+        document.querySelectorAll(".addedContentHobbies")[0].classList.toggle("added-content");
+        document.querySelectorAll(".jsAddedContentAboutMeAdjust")[0].classList.toggle("jsAddedContentAboutMe");
+        document.querySelectorAll("#jsSectionAddContentAdjust")[0].classList.toggle("colored-section");
       break;
 
       case "See My Contacts!":
-        document.querySelectorAll(".addedContentContacts")[0].classList.remove("added-content");
-        document.querySelectorAll(".jsAddedContentAboutMe")[0].classList.remove("jsAddedContentAboutMe");
+        document.querySelectorAll(".addedContentContacts")[0].classList.toggle("added-content");
+        document.querySelectorAll(".jsAddedContentAboutMeAdjust")[0].classList.toggle("jsAddedContentAboutMe");
+        document.querySelectorAll("#jsSectionAddContentAdjust")[0].classList.toggle("colored-section");
       break;
 
       case "See My Skills!":
-        document.querySelectorAll(".addedContentSkills")[0].classList.remove("added-content");
-        document.querySelectorAll(".jsAddedContentAboutMe")[0].classList.remove("jsAddedContentAboutMe");
+        document.querySelectorAll(".addedContentSkills")[0].classList.toggle("added-content");
+        document.querySelectorAll(".jsAddedContentAboutMeAdjust")[0].classList.toggle("jsAddedContentAboutMe");
+        document.querySelectorAll("#jsSectionAddContentAdjust")[0].classList.toggle("colored-section");
       break;
 
       default:console.log(this.innerHTML)
@@ -27,13 +42,3 @@ for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
 
 
 }
-
-
-$("#jsPasswordCheck").click(function(){
-  var givenPassword = prompt("Enter Password")
-  if (givenPassword == "idk"){
-     window.location.href = "https://legendary-ui.github.io/cv/discord.html"
-  } else{
-    alert("access denied")
-  }
-})
